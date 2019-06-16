a1ef336442d48c1e8e7b2fce7a465582c145d744
saitt0055 <csai@traveltripper.com>
2019-06-16 20:34:28 +0530

    react hoooks added

--- src/components/comments/comment.js
+++ src/components/comments/comment.js
@@ -1,86 +1,32 @@
-import React, {Component} from 'react';
-import './comment.css';
+import React, {useState, useEffect} from 'react';
 import { Button } from '@material-ui/core';
 
-class Comment extends Component {
+function Comment() {
+    const [count, setCount] = useState(0);
 
-    constructor(props){
-        super(props);
-        console.log('constructor - called');
-        this.inc = this.inc.bind(this);
-        this.dec = this.dec.bind(this);
-        this.state = {
-            count: 0,
-        };
-    }
-
-    inc() {
-        this.setState((state) => {
-            return state.count = state.count + 1;
-        });
-    }
-
-    dec() {
-        this.setState((state) => {
-           return state.count = state.count - 1;
-        });
-    }
-
-    componentWillReceiveProps() {
-        console.log('component will receive props');
-    }
-
-
-    UNSAFE_componentWillReceiveProps() {
-        console.log('unsafe component will receive props'); 
-    }
-
-    componentWillMount() {
-        console.log('component will mount');
-    }
-
-    UNSAFE_componentWillMount(){
-        console.log('unsafe component will mount');
-    }
-
-    componentDidMount() {
-        console.log('component did mount');
-        window.document.title = 'you clicked 0 times'
-    }
-
-    shouldComponentUpdate() {
-        console.log('should component update'); 
-        return this.state.count;
-    }
-
-    componentWillUpdate(){
-        console.log('component will updated');
-    }
-
-    UNSAFE_componentWillUpdate(){
-        console.log('unsafe component will update');
-    }
-
-    componentDidUpdate() {
-        console.log('component did update');
-    }
-
-    componentWillUnmount() {
-        console.log('component un mount');
-    }
-
-    componentDidCatch() {
-        console.log('component did catch');
-    }
+    useEffect(() => {
+        window.document.title = `Clicked ${count} times`;
+        inc()
+        dec()
+    }, []);
     
-    render() {
+    const inc = () => {
+        window.document.title = `Clicked ${count} times`;
+        setCount(count + 1);
+    };
+
+    const dec = () => {
+        window.document.title = `Clicked ${count} times`;
+        setCount(count - 1);
+    }
+
         return (<div className="commentpad">
             <h1>Please comment here</h1>
-            <span>{ this.state.count }</span>
-            <Button onClick={this.inc}> Increment </Button>
-            <Button onClick={this.dec}> Decrement </Button>
+            <span>{ count }</span>
+            <Button onClick={inc}> Increment </Button>
+            <Button onClick={dec}> Decrement </Button>
         </div>)
-    }
+    
 }
 
 export default Comment;
--- src/components/items/Items.js
+++ src/components/items/Items.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import { Link } from "react-router-dom";
 
 function Items() {
+    
     useEffect(() => {
         fetchItems();
     }, []);
