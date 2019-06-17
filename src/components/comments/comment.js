68ebbe84da7c6e98bea094eb2dc45eafd0a1b840
saitt0055 <csai@traveltripper.com>
2019-06-17 16:09:03 +0530

    rxjs added in react app

--- src/components/comments/comment.js
+++ src/components/comments/comment.js
@@ -1,7 +1,20 @@
 import React, {useState} from 'react';
 import { Button } from '@material-ui/core';
+import {Subject} from 'rxjs/Rx';
 
 function Comment() {
+    const subject = new Subject();
+
+      subject.subscribe({
+         next: (v) => console.log('observerA: ' + v)
+      });
+      subject.subscribe({
+        next: (v) => console.log('observerB: ' + v)
+      });
+      
+      subject.next(1);
+      subject.next(2);
+
     const [count, setCount] = useState(0);
         return (<div className="commentpad">
             <h1>Please comment here</h1>
