b8c758edc19414f4fabd89bf6ad4368d21a9cc81
saitt0055 <csai@traveltripper.com>
2019-06-17 15:59:09 +0530

    use state added

--- src/components/comments/comment.js
+++ src/components/comments/comment.js
@@ -1,30 +1,13 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import { Button } from '@material-ui/core';
 
 function Comment() {
     const [count, setCount] = useState(0);
-
-    useEffect(() => {
-        window.document.title = `Clicked ${count} times`;
-        inc()
-        dec()
-    }, []);
-    
-    const inc = () => {
-        window.document.title = `Clicked ${count} times`;
-        setCount(count + 1);
-    };
-
-    const dec = () => {
-        window.document.title = `Clicked ${count} times`;
-        setCount(count - 1);
-    }
-
         return (<div className="commentpad">
             <h1>Please comment here</h1>
             <span>{ count }</span>
-            <Button onClick={inc}> Increment </Button>
-            <Button onClick={dec}> Decrement </Button>
+            <Button onClick={() => setCount(count + 1)}> Increment </Button>
+            <Button onClick={() => setCount(count - 1)}> Decrement </Button>
         </div>)
     
 }
